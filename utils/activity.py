bcca3b7a6f5dfc511ecc66c601e2a0a2d66a4ad1
Marina Nelson <mjnelson@wpi.edu>
2025-01-14 10:35:46 -0500

    add signature to activity type enum fallback

--- utils/activity.py
+++ utils/activity.py
@@ -10,9 +10,10 @@ class Club9ActivityType(Enum):
     NONE = None                                     # A placeholder value for quests that have not yet been loaded.
     CHALLENGE = "CHALLENGE"                         # Users complete the activity by completing several quests.
     QUEST_EXTERNAL = "NON_INTEGRATION_CUSTOM"       # Users complete the activity external to the 'https://store.cloud9.gg/pages/club9' page.
-    QUEST_INTERNAL = "DEFAULT"                              # Users complete the activity on the 'https://store.cloud9.gg/pages/club9' page.
+    QUEST_INTERNAL = "DEFAULT"                      # Users complete the activity on the 'https://store.cloud9.gg/pages/club9' page.
 
-    
+
+    @classmethod
     def _missing_(cls, value):
         """
         Unknown values are mapped to 'QUEST_INTERNAL' as a fallback.
@@ -34,7 +35,7 @@ class Club9ActivityData():
         self.club9_activity_data = data
         for k, v in self.get_activity_data(data=self.club9_activity_data).items():
             setattr(self, k, v)
-        self.club9_activity_type = Club9ActivityType.NONE
+        self.club9_activity_type = self.get_activity_type()
         # TODO add logic to decompose the data input, identify the activity type, and assign the object a type with the corresponding method
 
 
@@ -69,11 +70,11 @@ class Club9ActivityData():
         """
         Gets the activity type by analyzing the activity data.
         """
-        try:
-            if (hasattr(self, "typeIdentifier") == False):
-                # TODO add logic in case the typeIdentifier attribute is missing (seems unlikely that it would ever be missing)
-                return Club9ActivityType.NONE
-            else:
+        if (hasattr(self, "typeIdentifier") == False):
+            # TODO add logic in case the typeIdentifier attribute is missing (seems unlikely that it would ever be missing)
+            return Club9ActivityType(None)
+        else:
+            return Club9ActivityType(self.typeIdentifier)
 
 
 
@@ -104,90 +105,90 @@ class Club9ActivityData():
 
 
 
-test = {
-    "id": "18111518-18f3-43e8-a96d-0b3d4e66effb",
-    "name": "[VAL] Watch C9 vs NRG - VCT Americas Kickoff",
-    "description": "",
-    "url": "",
-    "image": "https://s3.buildwithtoki.com/files/v0/sandbox/79990613-eac4-4477-bb3e-93b4a46a2958.png",
-    "iconImage": "",
-    "rewardMessage": "XP",
-    "additionalFields": {
-        "shopIds": [],
-        "rewardName": "",
-        "description": "",
-        "displayRule": {
-            "name": "[VAL] Watch C9 vs NRG - VCT Americas Kickoff",
-            "image": "https://s3.buildwithtoki.com/files/v0/sandbox/79990613-eac4-4477-bb3e-93b4a46a2958.png",
-            "custom": False,
-            "iconImage": "",
-            "description": ""
-        },
-        "buttonSettings": {
-            "enabled": False
-        },
-        "isOneTimeReward": True,
-        "visibilitySettings": {
-            "type": "all_users"
-        },
-        "image": "https://s3.buildwithtoki.com/files/v0/sandbox/79990613-eac4-4477-bb3e-93b4a46a2958.png",
-        "closed_at": "2025-01-18T06:00:36.930Z",
-        "iconImage": "",
-        "started_at": None,
-        "externalUrl": "https://store.cloud9.gg/pages/video?slug=vct-americas-kickoff-c9-vs-nrg",
-        "preventReSubmissions": False
-    },
-    "reward": {
-        "reward_rule_id": "8d974481-f5e7-459e-9a90-3c6bfd9eef7c",
-        "activity_id": "18111518-18f3-43e8-a96d-0b3d4e66effb",
-        "reward_type": "asset",
-        "asset_to_reward_id": "6e23ba5d-868e-496e-bc80-ba8f75e671e0",
-        "amount_to_reward": None,
-        "amount_to_reward_percentage": None,
-        "product_variant_id": None,
-        "discount_absolute": None,
-        "discount_percent": None,
-        "segment_id": None,
-        "order_number": 0,
-        "additional_fields": {
-            "shopIds": [],
-            "rewardName": "",
-            "description": "",
-            "displayRule": {
-                "name": "[VAL] Watch C9 vs NRG - VCT Americas Kickoff",
-                "image": "https://s3.buildwithtoki.com/files/v0/sandbox/79990613-eac4-4477-bb3e-93b4a46a2958.png",
-                "custom": False,
-                "iconImage": "",
-                "description": ""
-            },
-            "buttonSettings": {
-                "enabled": False
-            },
-            "isOneTimeReward": True,
-            "visibilitySettings": {
-                "type": "all_users"
-            }
-        },
-        "tier_id": None,
-        "product_variant_ids": None,
-        "token_name": "XP",
-        "token_id": "6e23ba5d-868e-496e-bc80-ba8f75e671e0",
-        "image_url": None,
-        "shopify_product_variant_label": None
-    },
-    "completed": False,
-    "limitReached": False,
-    "pendingReview": False,
-    "automaticallyApprove": True,
-    "retroactiveSubmission": False,
-    "maxFilesLimit": None,
-    "typeIdentifier": "NON_INTEGRATION_CUSTOM",
-    "completionsLimit": {
-        "type": "unlimited"
-    },
-    "products": None
-}
+# test = {
+#     "id": "18111518-18f3-43e8-a96d-0b3d4e66effb",
+#     "name": "[VAL] Watch C9 vs NRG - VCT Americas Kickoff",
+#     "description": "",
+#     "url": "",
+#     "image": "https://s3.buildwithtoki.com/files/v0/sandbox/79990613-eac4-4477-bb3e-93b4a46a2958.png",
+#     "iconImage": "",
+#     "rewardMessage": "XP",
+#     "additionalFields": {
+#         "shopIds": [],
+#         "rewardName": "",
+#         "description": "",
+#         "displayRule": {
+#             "name": "[VAL] Watch C9 vs NRG - VCT Americas Kickoff",
+#             "image": "https://s3.buildwithtoki.com/files/v0/sandbox/79990613-eac4-4477-bb3e-93b4a46a2958.png",
+#             "custom": False,
+#             "iconImage": "",
+#             "description": ""
+#         },
+#         "buttonSettings": {
+#             "enabled": False
+#         },
+#         "isOneTimeReward": True,
+#         "visibilitySettings": {
+#             "type": "all_users"
+#         },
+#         "image": "https://s3.buildwithtoki.com/files/v0/sandbox/79990613-eac4-4477-bb3e-93b4a46a2958.png",
+#         "closed_at": "2025-01-18T06:00:36.930Z",
+#         "iconImage": "",
+#         "started_at": None,
+#         "externalUrl": "https://store.cloud9.gg/pages/video?slug=vct-americas-kickoff-c9-vs-nrg",
+#         "preventReSubmissions": False
+#     },
+#     "reward": {
+#         "reward_rule_id": "8d974481-f5e7-459e-9a90-3c6bfd9eef7c",
+#         "activity_id": "18111518-18f3-43e8-a96d-0b3d4e66effb",
+#         "reward_type": "asset",
+#         "asset_to_reward_id": "6e23ba5d-868e-496e-bc80-ba8f75e671e0",
+#         "amount_to_reward": None,
+#         "amount_to_reward_percentage": None,
+#         "product_variant_id": None,
+#         "discount_absolute": None,
+#         "discount_percent": None,
+#         "segment_id": None,
+#         "order_number": 0,
+#         "additional_fields": {
+#             "shopIds": [],
+#             "rewardName": "",
+#             "description": "",
+#             "displayRule": {
+#                 "name": "[VAL] Watch C9 vs NRG - VCT Americas Kickoff",
+#                 "image": "https://s3.buildwithtoki.com/files/v0/sandbox/79990613-eac4-4477-bb3e-93b4a46a2958.png",
+#                 "custom": False,
+#                 "iconImage": "",
+#                 "description": ""
+#             },
+#             "buttonSettings": {
+#                 "enabled": False
+#             },
+#             "isOneTimeReward": True,
+#             "visibilitySettings": {
+#                 "type": "all_users"
+#             }
+#         },
+#         "tier_id": None,
+#         "product_variant_ids": None,
+#         "token_name": "XP",
+#         "token_id": "6e23ba5d-868e-496e-bc80-ba8f75e671e0",
+#         "image_url": None,
+#         "shopify_product_variant_label": None
+#     },
+#     "completed": False,
+#     "limitReached": False,
+#     "pendingReview": False,
+#     "automaticallyApprove": True,
+#     "retroactiveSubmission": False,
+#     "maxFilesLimit": None,
+#     "typeIdentifier": "NON_INTEGRATION_CUSTOM",
+#     "completionsLimit": {
+#         "type": "unlimited"
+#     },
+#     "products": None
+# }
 
 
-idk = Club9ActivityData(test)
-idk.show()
+# idk = Club9ActivityData(test)
+# print(idk.club9_activity_type)
